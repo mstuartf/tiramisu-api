c23d671eed203c6043ffa5de5f86147d2aaf43da
Mike Flood <mstuartflood@gmail.com>
2023-03-12 10:23:52 +0000

    fetching profile from proxycurl

--- project/apps/prospects/views.py
+++ project/apps/prospects/views.py
@@ -3,6 +3,8 @@ import logging
 from rest_framework import mixins, status
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from .models import Prospect
+from .proxycurl import fetch_profile_request
 
 from .serializers import (
     WriteProspectSerializer,
@@ -20,14 +22,26 @@ class ProspectView(
     serializer_class = ReadProspectSerializer
 
     def create(self, request, *args, **kwargs):
-        data = {
-            **request.data,
-            "user": request.user.id,
-            "raw": {},
-        }
-        serializer = WriteProspectSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        obj = serializer.save()
+        slug = request.data["slug"]
+        logger.info("received request for slug {}".format(slug))
+        try:
+            obj = Prospect.objects.get(slug=slug)
+        except Prospect.DoesNotExist:
+            logger.info("no profile in db with this slug")
+            raw = fetch_profile_request(slug)
+            data = {
+                "slug": slug,
+                "user": request.user.id,
+                "first_name": raw.get("first_name"),
+                "last_name": raw.get("last_name"),
+                "headline": raw.get("headline"),
+                "summary": raw.get("summary"),
+                "raw": raw,
+            }
+            serializer = WriteProspectSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            obj = serializer.save()
+
         read_serializer = ReadProspectSerializer(obj)
         headers = self.get_success_headers(read_serializer.data)
         return Response(
