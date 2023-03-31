69db0bf20e5e20cef4826e0678d6445aaa67042f
Mike Flood <mstuartflood@gmail.com>
2023-03-31 10:23:58 +0100

    salesforce auth links

--- project/apps/companies/admin.py
+++ project/apps/companies/admin.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib import admin
 from .models import Company
 
@@ -9,4 +11,13 @@ class CompanyAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "name",
+        "salesforce_link",
     )
+
+    def salesforce_link(self, obj):
+        return 'https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=' \
+               '{client_id}&redirect_uri={redirect_uri}&state={state}'.format(
+            client_id=os.environ["SALESFORCE_CONSUMER_KEY"],
+            redirect_uri=os.environ["SALESFORCE_CALLBACK_URL"],
+            state=str(obj.id),
+        )
