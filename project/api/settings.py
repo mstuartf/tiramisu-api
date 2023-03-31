02a8b0f6cdfc5bc815148c207384b65fec09de15
Mike Flood <mstuartflood@gmail.com>
2023-03-31 09:42:10 +0100

    refresh token timeline

--- project/api/settings.py
+++ project/api/settings.py
@@ -101,6 +101,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True
 }
 
